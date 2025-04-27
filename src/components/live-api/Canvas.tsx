b11308febb4508e2ed1e68b42f1db34e2f83d976
Clueed <7318830+Clueed@users.noreply.github.com>
2025-04-27 09:00:16 +0200

    update canvas

--- src/components/live-api/Canvas.tsx
+++ src/components/live-api/Canvas.tsx
@@ -4,19 +4,23 @@ import { Editor, EditorContainer } from '@/components/plate-ui/editor';
 import { Plate } from '@udecode/plate/react';
 
 import { useCreateEditor } from '../editor/use-create-editor';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 interface CanvasProps {
-    editor: ReturnType<typeof useCreateEditor>;
+  editor: ReturnType<typeof useCreateEditor>;
 }
 
 const CanvasComponent = React.memo(function CanvasComponent({ editor }: CanvasProps) {
-    return (
-        <Plate editor={editor}>
-            <EditorContainer>
-                <Editor variant='demo' placeholder='Type...' />
-            </EditorContainer>
-        </Plate>
-    );
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Plate editor={editor}>
+        <EditorContainer>
+          <Editor variant='default' />
+        </EditorContainer>
+      </Plate>
+    </DndProvider>
+  );
 });
 
 export const Canvas = CanvasComponent;
